eea80c629d422dddd878de24cd7d499baab3d657
Marc Lindenbach <marc.lindenbach@gmail.com>
2019-09-02 11:59:27 -0600

    Added function to return weather as csv

--- src/get-weather.js
+++ src/get-weather.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const getWeatherUrl = (stationId, year, month, day) => `http://climate.weather.gc.ca/climate_data/bulk_data_e.html
   ?format=csv
   &stationID=${stationId}
@@ -7,6 +9,11 @@ const getWeatherUrl = (stationId, year, month, day) => `http://climate.weather.g
   &timeframe=1
   &submit=Download+Data`.replace(/\n *\ /g, '');
 
+const getWeather = async (stationId, year, month, day) => {
+  const { data } = await axios.get(getWeatherUrl(stationId, year, month, day));
+  console.log(data);
+};
+
 module.exports = {
-  getWeatherUrl,
+  getWeather,
 };
